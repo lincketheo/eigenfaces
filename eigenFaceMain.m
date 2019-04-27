6da5bb9093b0cec1bb62af0954ec0980a9494da6
tlincke125 <tlincke125@gmail.com>
2019-04-26 21:00:54 -0600

    cleaned main compression

--- eigenFaceMain.m
+++ eigenFaceMain.m
@@ -1,33 +1,41 @@
-function eigenFaceMain(numFaces, file, folderPath)
+function eigenFaceMain(numSV, file, folderPath)
    %A is 1808000 x 130
    A = loopThroughFile(folderPath);
-   Avg = mean(A, 2);
-      
-   %size(A)
-   %B is 1808000 x 1
-   B = reshape(double(imread(file)), 784, 1);
-   [U, ~, ~] = svd(double(A), 'econ');
+   size(A)
+   [U, Avg] = produceFaceBasis(A);
+   clear A;
+   size(U)
+   origional = imread(file);
+   if length(size(origional)) == 3
+       origional = rgb2gray(origional);
+   end
    
-   B = U(:,1:numFaces) * (U(:,1:numFaces).' * (B - Avg)) + Avg;
+   [d1, d2] = size(origional);
+   clear origional;
+   recreatedImage = recreateFace(numSV, U, file, Avg);
    
-   %B = U * U.' * (B - Avg) + Avg;
-
-   imshow(uint8(reshape(B, 28, 28)));
+   imageSVDShow(U, Avg, numSV, recreatedImage, d1, d2)
 end
+
 
 
 %returns a matrix with all the n x m image matrices (must be the same size)
 %as (m * n) x 1 vectors in as cols
 function [faceMatrix] = loopThroughFile(folderPath)
+    %loops through a given directory to collect all images and but into
+    %one large matrix where cols are the images as vectors
+    
+    %@param folderPath the folder to loop through
+    %@return faceMatrix a matrix with all cols vertical vectors of
+    %m * n x 1 dimension where each image is m x n
     a = dir(fullfile(folderPath,'*.jpg'));
     fileNames = {a.name};
     faceMatrix = [];
-    
-    %loops through all files ad them to the matrix
     for k = 1:length(fileNames)
             filename = string(fileNames(k));
             filename = strcat(folderPath, filename);
             I = (imread(filename));
+            I = rgb2gray(I);
             [a, b] = size(I);
             I = reshape(I, [a * b, 1]);
             %possibility of dim missmatch
@@ -35,10 +43,30 @@ function [faceMatrix] = loopThroughFile(folderPath)
     end
 end
 
+function [U, Avg] = produceFaceBasis(faceMatrix)
+    Avg = mean(faceMatrix, 2);
+    faceMatrix = double(faceMatrix) - double(Avg);
+    [U, ~, ~] = svd(double(faceMatrix), 'econ');
+end
+
+function [image] = recreateFace(numberEigenFaces, U, faceFile, Avg)
+    image = imread(faceFile);
+    if length(size(image)) == 3
+        image = rgb2gray(image);
+    end
+    image = double(reshape(image.', 1, [])).'; 
+    
+    image = U(:,1:numberEigenFaces) * (U(:,1:numberEigenFaces).' * (image - Avg)) + Avg;
+end
+
+function accuracy = getAccuracy(image, constructedImage)
+    accuracy = ((abs(norm(image) - norm(constructedImage))) / norm(image));
+end
 
 
-function imageSVDShow(s,v,d)
-    imshow(uint8(s * diag(v) * d.'));
+function imageSVDShow(U, avg, numFaces, image, d1, d2)
+    image = U(:,1:numFaces) * (U(:,1:numFaces).' * (image - avg)) + avg;
+    imshow(uint8(reshape(image, d1, d2)));
 end
 
 
